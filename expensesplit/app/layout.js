c82bf1c10338c398bad0f1a0f535c1d5abeb9deb
ananya.jha <ananya.jha@maximl.com>
2025-10-14 01:32:25 -0400

    initial nextjs + shadcn setup

---
+++ expensesplit/app/layout.js
@@ -0,0 +1,37 @@
+import { Inter} from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/convex-client-proder";
+const inter = Inter({subsets:["latin"]});
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Expense Splitting Application",
+  description: "Split your bills efficiently",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href='/logos/logo-s.png' sizes="any"/>
+      </head>
+      <body className={`${inter.className} `}>
+        <ConvexClientProvider>
+          <Header/>
+          <main className="min-h-screen">{children}</main>
+
+        </ConvexClientProvider>
+        
+      </body>
+    </html>
+  );
+}
