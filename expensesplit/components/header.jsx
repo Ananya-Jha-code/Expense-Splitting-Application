3939cd09a62b85356a2c165d4b9019152d0ea912
ananya.jha <ananya.jha@maximl.com>
2025-10-19 16:13:42 -0400

    Authentication Routes

--- expensesplit/components/header.jsx
+++ expensesplit/components/header.jsx
@@ -1,5 +1,35 @@
+"use client";
+
 import React from "react";
-const Header = () =>{
-    return <div> Header</div>;
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center p-4 bg-white shadow">
+      <h1 className="text-lg font-bold">Expense Splitter</h1>
+
+      <div className="flex gap-3 items-center">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </div>
+    </div>
+  );
 };
-export default Header
+
+export default Header;
